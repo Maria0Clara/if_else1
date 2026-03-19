24970a60b2e5f4347071baad9626a1db883ff1f3
mariaclaradossantoschaves@gmail.com <Maria0Clara>
2026-03-19 20:15:02 -0300

    salvo

--- if_else.py
+++ if_else.py
@@ -1,4 +1,5 @@
 #1
+
 print(1, True and True)
 print(2, False and True)                    
 print(3, 1 == 1 and 2 == 1)                  
@@ -20,6 +21,9 @@ print(18, 1 == 1 and (not ("testing" == 1 or 1 == 0)))
 print(19, "chunky" == "bacon" and (not (3 == 4 or 3 == 3)))      
 print(20, 3 == 3 and (not ("testing" == "testing" or "Python" == "Fun")))
 
+
+
+
 #2
 
 num = int(input("Digite um número: "))
@@ -29,6 +33,8 @@ if num >= 0:
 else:
     print("Número negativo")
     
+
+
 
 #3
 
@@ -42,4 +48,75 @@ if valor_parcela <= salario * 0.3:
     print("Empréstimo aprovado")
 else:
     print("Empréstimo negado")
+    
+    
+    
+    
+#4
+num = int(input("Digite um número: "))
+
+if num % 2 == 0:
+    print("Par")
+else:
+    print("Ímpar")
+    
+
+
+#5
+
+valor_hora = float(input("Valor da hora: "))
+horas = float(input("Horas trabalhadas: "))
+inss = float(input("Percentual INSS (%): "))
+
+salario_bruto = valor_hora * horas
+desconto = salario_bruto * (inss / 100)
+salario_liquido = salario_bruto - desconto
+
+print("Salário bruto:", salario_bruto)
+print("Salário líquido:", salario_liquido)
+
+
+
+#6
+a = float(input("Digite A: "))
+b = float(input("Digite B: "))
+
+if a > b:
+    print("Maior:", a)
+else:
+    print("Maior:", b)
+
+
+
+#7
+
+p1 = float(input("Nota 1: "))
+p2 = float(input("Nota 2: "))
+p3 = float(input("Nota 3: "))
+
+media = (p1*2 + p2*3 + p3*5) / 10
+
+print("Média final:", media)
+
+
+
+
+#16
+
+nome = input("Nome do candidato: ")
+
+port = float(input("Nota Português: "))
+mat = float(input("Nota Matemática: "))
+geral = float(input("Nota Conhecimentos Gerais: "))
+
+media = (port + mat + geral) / 3
+
+print("\nNome:", nome)
+print("Notas:", port, mat, geral)
+print("Média:", media)
+
+if media > 7 and port >= 5 and mat >= 5 and geral >= 5:
+    print("Aprovado")
+else:
+    print("Reprovado")
 
