eaef93e8aeffe1ef1a14a70f5f4af10ab5873465
mariaclaradossantoschaves@gmail.com <Maria0Clara>
2026-03-19 20:05:03 -0300

    salvos1

--- if_else.py
+++ if_else.py
@@ -1,3 +1,4 @@
+#1
 print(1, True and True)
 print(2, False and True)                    
 print(3, 1 == 1 and 2 == 1)                  
@@ -20,4 +21,25 @@ print(19, "chunky" == "bacon" and (not (3 == 4 or 3 == 3)))
 print(20, 3 == 3 and (not ("testing" == "testing" or "Python" == "Fun")))
 
 #2
+
+num = int(input("Digite um número: "))
+
+if num >= 0:
+    print("Número positivo")
+else:
+    print("Número negativo")
+    
+
+#3
+
+valor = float(input("Valor do empréstimo: "))
+parcelas = int(input("Número de parcelas: "))
+salario = float(input("Salário: "))
+
+valor_parcela = valor / parcelas
+
+if valor_parcela <= salario * 0.3:
+    print("Empréstimo aprovado")
+else:
+    print("Empréstimo negado")
 
